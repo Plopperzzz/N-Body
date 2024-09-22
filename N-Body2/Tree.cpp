a0df6ed3e204bee7c3038a118415ba1082a262c0
Christian Anderson <christian.anderson982@gmail.com>
2024-09-22 11:42:00 -0400

    delete commented code for Node in Tree.cpp

--- N-Body2/Tree.cpp
+++ N-Body2/Tree.cpp
@@ -1,31 +1,6 @@
 #include "Tree.h"
 #include <iostream>
 
-
-// Node
-//Node::Node():
-//	m_id(-1)
-//{}
-//
-//Node::Node(int id, std::string name, glm::dvec3 position, glm::dvec3 velocity, double const mass, double const radius) :
-//	m_id(id),
-//	position(position),
-//	velocity(velocity),
-//	force(glm::dvec3(0)),
-//	name(name),
-//	mass(mass),
-//	radius(radius)
-//	//path(std::make_shared<std::vector<int>>())
-//{}
-//
-//int Node::getId() {
-//	return m_id;
-//}
-//
-//void Node::setId(int id) {
-//	m_id = id;
-//}
-//
 const glm::dvec3 OctTree::basis[8] = {
 	glm::dvec3(-1, -1, -1), // Index 0: bSOUTHWEST
 	glm::dvec3(1, -1, -1),  // Index 1: bSOUTHEAST
