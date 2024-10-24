2ca36e0b0eb6ba135f23b31c50132fe1c7ae92e2
SB <christian.anderson982@gmail.com>
2024-10-24 15:40:45 -0400

    Adding more callbacks

--- N-Body2/OpenGL/Window.h
+++ N-Body2/OpenGL/Window.h
@@ -44,51 +44,18 @@ private:
 	void createWindow();
 	void cleanup();
 
-	static void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-
-		if (action == GLFW_PRESS || action == GLFW_REPEAT) {
-			if (key == GLFW_KEY_W || key == GLFW_KEY_S || key == GLFW_KEY_A || key == GLFW_KEY_D) {
-
-				//	if (key == GLFW_KEY_W) {
-				//		Position += speed * Up; // Move up
-				//	}
-				//	if (key == GLFW_KEY_S) {
-				//		Position -= speed * Up; // Move down
-				//	}
-				//	if (key == GLFW_KEY_A) {
-				//		Position -= speed * glm::normalize(glm::cross(Orientation, Up)); // Move left
-				//	}
-				//	if (key == GLFW_KEY_D) {
-				//		Position += speed * glm::normalize(glm::cross(Orientation, Up)); // Move right
-				//	}
-				//// Update orthographic projection after panning
-				//SetOrthographic(orthoLeft + Position.x, orthoRight + Position.x,
-				//	orthoBottom + Position.y, orthoTop + Position.y,
-				//	nearPlane, farPlane);
-			}
-		}
-		if (key == GLFW_KEY_SPACE)
-		{
-			Camera* cam = static_cast<Camera*>(glfwGetWindowUserPointer(window));
-			cam->debug = !cam->debug;
-			return;
-		}
+	static void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+	{
+	}
+	static void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
+	{
+	}
+	static void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset)
+	{
 	}
 
-	// Mouse button callback to handle middle mouse button press
-	static void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods) {
-		Camera* cam = static_cast<Camera*>(glfwGetWindowUserPointer(window));
-		if (button == GLFW_MOUSE_BUTTON_MIDDLE) {
-			if (action == GLFW_PRESS) {
-				cam->middleMousePressed = true;
-				double xpos, ypos;
-				glfwGetCursorPos(window, &xpos, &ypos);
-				cam->lastMousePosition = glm::vec2(xpos, ypos);
-			}
-			else if (action == GLFW_RELEASE) {
-				cam->middleMousePressed = false;
-			}
-		}
+	void CursorPositionCallback(GLFWwindow* window, double xpos, double ypos)
+	{
 	}
 
 };
